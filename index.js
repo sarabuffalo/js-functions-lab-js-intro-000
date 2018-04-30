74af342358c1e9314061ba2282d43317754ee203
sarabuffalo <sep.14094@yahoo.com>
2018-04-30 00:34:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
   }
+
+function happyHolidayTo(h, n) {
+  return `Happy ${h}, ${n}!`
+}
+
+holidayCountdown(holiday, day) {
+  return `It's ${day} days until ${holiday}!`
+}
